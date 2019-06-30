e5eaa303bd4ba44e129f3ed487544f3a10640bf1
prrao87 <prrao87@gmail.com>
2019-06-30 16:17:39 -0400

    Update args

--- run_classifiers.py
+++ run_classifiers.py
@@ -69,13 +69,13 @@ if __name__ == "__main__":
     method_list = [method for method in METHODS.keys()]
     # Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-tr', '--train', type=str, help="Path (str) to train data file", default=TRAIN_PATH)
-    parser.add_argument('-d', '--dev', type=str, help="Path (str) to dev data file", default=DEV_PATH)
-    parser.add_argument('-te', '--test', type=str, help="Path (str) to test data file", default=TEST_PATH)
+    parser.add_argument('-t', '--train', type=str, help="Train data file (str)", default=TRAIN_PATH)
+    parser.add_argument('-d', '--dev', type=str, help="Dev data file (str)", default=DEV_PATH)
+    parser.add_argument('-v', '--val', type=str, help="Test/Validation data file (str)", default=TEST_PATH)
     parser.add_argument('-m', '--method', type=str, nargs='+', help="Enter one or more methods \
                         (Choose from following: {})".format(", ".join(method_list)),
                         required=True)
-    parser.add_argument('-f', '--model', type=str, help="Path (str) to trained classifier model file", default=None)
+    parser.add_argument('-f', '--model', type=str, help="Trained classifier model file (str)", default=None)
     parser.add_argument('-l', '--lower', action="store_true", help="Flag to convert test data strings \
                         to lower case (for lower-case trained classifiers)")
     args = parser.parse_args()

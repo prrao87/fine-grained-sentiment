53e6602b8b7a5ca656e759eff570a277bc0f8702
prrao87 <prrao87@gmail.com>
2019-07-15 22:17:30 +0100

    Add argument parser to explainer

--- explainer.py
+++ explainer.py
@@ -1,24 +1,36 @@
+import argparse
 import lime.lime_text
 import numpy as np
 import webbrowser
 from pathlib import Path
 
 
-def tokenize_string(text):
-    return text.split()
+METHODS = {
+    'fasttext': "models/fasttext/sst.bin",
+    'flair': "models/flair/best-model-elmo.pt"
+}
 
 
-def fasttext_pred_formatted(model_file, text):
+def load_fasttext(path_to_model):
+    "Load FastText classifier"
+    import fasttext
+    classifier = fasttext.load_model(path_to_model)
+    return classifier
+
+
+def load_flair(path_to_model):
+    "Load Flair classifier"
+    from flair.models import TextClassifier
+    classifier = TextClassifier.load(path_to_model)
+    return classifier
+
+
+def fasttext_predictor(classifier, text):
     """Generate an array of predicted labels/scores using FastText
     """
-    import fasttext
-
-    # Load fasttext classifier
-    classifier = fasttext.load_model(model_file)
     labels, probs = classifier.predict(text, 5)
 
     # For each prediction, sort the probability scores in the same order for all texts
-    # (fasttext by default returns predictions in the most likely order for each text)
     result = []
     for label, prob, txt in zip(labels, probs, text):
         order = np.argsort(np.array(label))
@@ -27,23 +39,18 @@ def fasttext_pred_formatted(model_file, text):
     return np.array(result)
 
 
-def flair_pred_formatted(model_file, text):
+def flair_predictor(classifier, text):
     """Generate an array of predicted labels/scores using the Flair NLP library
     """
-    from flair.models import TextClassifier
     from flair.data import Sentence
-
-    # Load flair model
-    model = TextClassifier.load(model_file)
     # Make prediction and sort probabilities
     doc = Sentence(text)
-    model.predict(doc, multi_class_prob=True)
+    classifier.predict(doc, multi_class_prob=True)
 
     labels = [x.value for x in doc.labels]
     probs = np.array([x.score for x in doc.labels])
 
     # For each prediction, sort the probability scores in the same order for all texts
-    # (flair by default returns predictions in the most likely order for each text)
     result = []
     for label, prob, txt in zip(labels, probs, text):
         order = np.argsort(np.array(label))
@@ -52,16 +59,24 @@ def flair_pred_formatted(model_file, text):
     return np.array(result)
 
 
-if __name__ == "__main__":
-    # Example string
-    sample = "The story loses its bite in a last-minute happy ending that 's even less plausible than the rest of the picture ."
+def main(method, path_to_model, text):
+    """Run LIME explainer on provided classifier
+    """
+    if method == "fasttext":
+        classifier = load_fasttext(path_to_model)
+        predictor = fasttext_predictor
+    elif method == "flair":
+        classifier = load_flair(path_to_model)
+        predictor = flair_predictor
+    else:
+        raise Exception("Requested method {} explainer funtction not implemented!")
 
     # Create a LimeTextExplainer
     explainer = lime.lime_text.LimeTextExplainer(
         # Specify split option
-        split_expression=tokenize_string,
-        # Our classifer uses bigrams (two-word pairs) to classify text, hence, order matters
-        # and we cannot use bag of words.
+        split_expression=lambda x: x.split(),
+        # Our classifer uses bigrams or contextual ordering to classify text
+        # Hence, order matters, and we cannot use bag of words.
         bow=False,
         # Specify class names for this case
         class_names=[1, 2, 3, 4, 5]
@@ -69,15 +84,39 @@ if __name__ == "__main__":
 
     # Make a prediction and explain it:
     exp = explainer.explain_instance(
-                    sample,
-                    # classifier_fn=lambda x: fasttext_pred_formatted('models/fasttext/sst.bin', x),
-                    classifier_fn=lambda x: flair_pred_formatted('models/flair/best-model-elmo.pt', x),
-                    top_labels=1,
-                    num_features=25,
-                )
+        text,
+        classifier_fn=lambda x: predictor(classifier, x),
+        top_labels=1,
+        num_features=10,
+    )
+    return exp
 
-    output_filename = Path(__file__).parent / "explanation.html"
-    exp.save_to_file(output_filename)
 
-    # Open the explanation html in our web browser.
-    # webbrowser.open(output_filename.as_uri())
+if __name__ == "__main__":
+
+    # Example string
+    sample = "It 's not horrible , just horribly mediocre ."
+
+    # Get list of available methods:
+    method_list = [method for method in METHODS.keys()]
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', type=str, nargs='+', help="Enter one or more methods \
+                        (Choose from following: {})".format(", ".join(method_list)),
+                        required=True)
+
+    args = parser.parse_args()
+
+    for method in args.method:
+        if method not in METHODS.keys():
+            parser.error("Please choose from existing methods! {}".format(", ".join(method_list)))
+        try:
+            path_to_model = METHODS[method]
+            # Run explainer function
+            exp = main(method, path_to_model, sample)
+
+            # Output to HTML
+            output_filename = Path(__file__).parent / "explanation-{}.html".format(method)
+            exp.save_to_file(output_filename)
+        except ValueError:
+            raise Exception("Could not find classifier model file for {}".format(method))

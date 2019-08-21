99babc7eddb9956d54eefcb02cff0a91e9bb8e23
Prashanth Rao <35005448+prrao87@users.noreply.github.com>
2019-08-21 13:12:30 -0400

    Add quantizer to reduce fasttext model size

--- training/train_fasttext.py
+++ training/train_fasttext.py
@@ -3,26 +3,35 @@ import os
 import fasttext
 
 
+def make_dirs(dirpath: str) -> None:
+    """Make directories for output if necessary"""
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
 def trainer(filepath: str,
             train_file: str,
-            model_file: str,
+            model_path: str,
             hyper_params: dict) -> None:
     """Train sentiment model using FastText:
     https://fasttext.cc/docs/en/supervised-tutorial.html
     """
     train = os.path.join(filepath, train_file)
-    output = os.path.join(filepath, model_file)
     model = fasttext.train_supervised(input=train, **hyper_params)
     print("FastText model trained with hyperparameters: \n {}".format(hyper_params))
-    # Save model
-    model.save_model(output)
+    # Save models to model directory for fasttext
+    make_dirs(model_path)
+    model.save_model(os.path.join(model_path, "sst-5.bin"))
+    # Quantize model to reduce space usage
+    model.quantize(input=train, qnorm=True, retrain=True, cutoff=100000)
+    model.save_model(os.path.join(model_path, "sst-5.ftz"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filepath', type=str, help="Dataset path", default="../data/sst")
     parser.add_argument('--train', type=str, help="Training set filename", default="sst_train.txt")
-    parser.add_argument('--model', type=str, help="Trained model output name", default="../models/sst.bin")
+    parser.add_argument('--model', type=str, help="Trained model path", default="../models/fasttext")
     parser.add_argument('--epochs', type=int, help="Number of epochs", default=100)
     parser.add_argument('--lr', type=float, help="Learning rate", default=0.5)
     parser.add_argument('--ws', type=int, help="Size of the context window", default=3)

7feedb4465cec58aa4c609481ea5ca8c9b762c97
prrao87 <prrao87@gmail.com>
2019-09-02 20:19:56 -0400

    Update hyperparams to optimum fasttext autotuned vals

--- training/train_fasttext.py
+++ training/train_fasttext.py
@@ -1,3 +1,15 @@
+"""
+Python API for FastText text classification model training.
+
+All default arguments in this file are obtained using hyperparameter autotuning
+in fastText: https://fasttext.cc/docs/en/autotune.html
+
+Run the following command for the CLI utility to obtain optimum hyperparameters:
+
+./fasttext supervised -input sst_train.txt -output model \
+-autotune-validation sst_dev.txt -autotune-modelsize 10M -verbose 3
+"""
+
 import argparse
 import os
 import fasttext
@@ -21,10 +33,10 @@ def trainer(filepath: str,
     print("FastText model trained with hyperparameters: \n {}".format(hyper_params))
     # Save models to model directory for fasttext
     make_dirs(model_path)
-    model.save_model(os.path.join(model_path, "sst-5.bin"))
+    model.save_model(os.path.join(model_path, "sst5_hyperopt.bin"))
     # Quantize model to reduce space usage
-    model.quantize(input=train, qnorm=True, retrain=True, cutoff=100000)
-    model.save_model(os.path.join(model_path, "sst-5.ftz"))
+    model.quantize(input=train, qnorm=True, retrain=True, cutoff=110539)
+    model.save_model(os.path.join(model_path, "sst5_hyperopt.ftz"))
 
 
 if __name__ == "__main__":
@@ -33,19 +45,26 @@ if __name__ == "__main__":
     parser.add_argument('--train', type=str, help="Training set filename", default="sst_train.txt")
     parser.add_argument('--model', type=str, help="Trained model path", default="../models/fasttext")
     parser.add_argument('--epochs', type=int, help="Number of epochs", default=100)
-    parser.add_argument('--lr', type=float, help="Learning rate", default=0.5)
-    parser.add_argument('--ws', type=int, help="Size of the context window", default=3)
+    parser.add_argument('--lr', type=float, help="Learning rate", default=0.35)
+    parser.add_argument('--ws', type=int, help="Size of the context window", default=5)
     parser.add_argument('--wordNgrams', type=int, help="Max length of word ngram", default=3)
-    parser.add_argument('--dim', type=int, help="Size of the word vectors", default=100)
+    parser.add_argument('--dim', type=int, help="Size of the word vectors", default=155)
+    parser.add_argument('--minn', type=int, help="Min length of char ngram", default=2)
+    parser.add_argument('--maxn', type=int, help="Max length of char ngram", default=5)
+    parser.add_argument('--bucket', type=int, help="Number of buckets", default=2014846)
 
     args = parser.parse_args()
 
+    # Use fastText command line utility to run hyperparameter optimization for best default args!
     hyper_params = {
         "lr": args.lr,
         "epoch": args.epochs,
         "wordNgrams": args.wordNgrams,
         "dim": args.dim,
         "ws": args.ws,
+        "minn": args.minn,
+        "maxn": args.maxn,
+        "bucket": args.bucket,
     }
 
     trainer(args.filepath, args.train, args.model, hyper_params)
